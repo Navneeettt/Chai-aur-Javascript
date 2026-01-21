3307ffee1df0360738d9b4fb1c934078b6c745a4
Navneet Mishra <navneet.mishra8586@gmail.com>
2026-01-21 23:57:33 +0530

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node JS, not browser
+
+console.log (3+3); console.log("Navneet") // If we want to write two statements in one line then we have to separate it by semicolon, but this is not recommeded
+// write readable code always
+
+let name = "Navneet"
+let age = 22
+let isloggedin = false
+let state;
+
+// number=> 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null=> standalone Value, intentionally empty
+// undefined => variable declared but value not defined
+// symbol=> unique
+// object
+
+console.log(typeof"Navneet")
+console.log(typeof null) // type of null is object
+console.log(typeof undefined) // type of undefined is undefined
