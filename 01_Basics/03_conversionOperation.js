7d41eb19cfafc99559ddcc72803a7ba744d5d46a
Navneet Mishra <navneet.mishra8586@gmail.com>
2026-01-22 21:44:51 +0530

    datatype

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,7 +1,10 @@
-let score = "33"
+// let score = "33"
+let score = "33sdfv"
 
 console.log(typeof score); //there are two methods of writing typeof, one is without parenthesis and is with 
-console.log(typeof (score));
+console.log(typeof (score)); 
 
-let valueInNumber = Number(score)
+let valueInNumber = Number(score) // it will convert the datatype to Number
 console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
