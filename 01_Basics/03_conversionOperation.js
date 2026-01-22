bc3a2ce29de0e3f74d5e3cc21d1ff90866ea692d
Navneet Mishra <navneet.mishra8586@gmail.com>
2026-01-22 22:26:19 +0530

    type of

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,14 +1,33 @@
-// let score = "33" // value= 33 
-// let score = "33sdfv" // value= NaN
-// let score = null //value= 0
-// let score = undefined //value=Nan
- let score = true // value= 1
-
-console.log(typeof score); //there are two methods of writing typeof, one is without parenthesis and is with 
-console.log(typeof (score)); 
+// let score = "33"       // value= 33 
+ let score = "Navneet"  // value = Nan
+// let score = "33sdfv"   // value= NaN
+// let score = null       //value= 0
+// let score = undefined  //value=Nan
+// let score = true        // value= 1
+// let score = false        // value = 0
+// console.log(typeof score); //there are two methods of writing typeof, one is without parenthesis and is with 
+// console.log(typeof (score)); 
 
 let valueInNumber = Number(score) // it will convert the datatype to Number
 
-console.log(typeof valueInNumber); //it checks the type
-console.log(valueInNumber) //it checks the number, if the type is number, but here there is not a number, there is 33sdfv, so it will show Not A Number
+// console.log(typeof valueInNumber); //it checks the type
+// console.log(valueInNumber) //it checks the number, if the type is number, but here there is not a number, there is 33sdfv, so it will show Not A Number
 
+let isLoggedIn = null
+let convert = Boolean(isLoggedIn)
+// console.log(convert)
+
+/* 
+null = false
+undefined = false
+"" = false
+0 = false
+"Navneet" = true
+1 = true
+any number greater than 0 = true
+*/
+
+let someNumber = 22
+let convertToString = String (someNumber)
+console.log(convertToString);
+console.log(typeof convertToString)
