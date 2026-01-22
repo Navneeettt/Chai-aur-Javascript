348bf96a769aa485a1863e2e1f42f2d43de3626b
Navneet Mishra <navneet.mishra8586@gmail.com>
2026-01-22 23:07:02 +0530

    true

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -49,12 +49,17 @@ let negvalue = -value
 
 let str1 = "Hello"
 let str2 = " Navneet"
-let str3 = str1 + str2
+let str3 = str1 + str2 // or we can directly write console.log(str1 + str2)
 console.log(str3)
 
 console.log("1" + 2);  // 12
 console.log(1 + "2");  // 12
 console.log("1" + "2");  //12
-console.log("1" + 2 + 2);  //122
+console.log("1" + 2 + 2);  //122 //if the first value is in string then it will treat all as string
 console.log(1 + 2 +"2");   //32
+console.log(1+2+null);    //3
 
+console.log(true);   //true
+console.log(+true);   // 1
+console.log(true+);    //error
+console.log(+"");     // 0
