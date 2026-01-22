18260aabf6bc418c4e0d4daca8d427825ba38ae8
Navneet Mishra <navneet.mishra8586@gmail.com>
2026-01-22 21:39:46 +0530

    conversion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,7 @@
+let score = "33"
+
+console.log(typeof score); //there are two methods of writing typeof, one is without parenthesis and is with 
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
