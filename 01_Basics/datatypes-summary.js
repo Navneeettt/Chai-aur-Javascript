6e6e93e41edd56aa46084dec09855d659cd77297
Navneet Mishra <navneet.mishra8586@gmail.com>
2026-01-23 23:00:24 +0530

    Primitive

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,20 @@
+// Primitive : These datatypes are call by their value, when we call it, we get the copy of that value, all changes happened in its copy, not in the original one
+
+/*
+7 Types:
+String
+Number
+Boolean
+Null
+Undefined
+Symbol
+BigInt
+*/
+
+// Reference (Non Primitive) :
+
+/*
+Array
+Object
+Functional
+*/
