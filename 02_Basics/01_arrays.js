41739609eaa243a0a26763044d411b34541b2bf0
Navneet Mishra <navneet.mishra8586@gmail.com>
2026-01-27 23:14:47 +0530

    slice

--- 02_Basics/01_arrays.js
+++ 02_Basics/01_arrays.js
@@ -12,14 +12,38 @@ console.log(myArr2.length);   //4
 
 // Array Methods
 
-myArr.push(6)  // It will add the element in last
+myArr.push(6)        // It will add the element in last
 console.log(myArr);   //[0, 1, 2, 3,4, 5, 6]
-myArr.pop()   // It will remove the last element
+myArr.pop()         // It will remove the last element
 console.log(myArr); //[ 0, 1, 2, 3, 4, 5 ]
 
-myArr.unshift(9)  // It will add the element in front/first position
+myArr.unshift(9)    // It will add the element in front/first position
 console.log(myArr);  
-myArr.shift()   // It will remove the first element
+myArr.shift()       // It will remove the first element
 
-console.log(myArr.includes(10)); // It checks whether the array element is present or not and gives uotput as true or false
-console.log(myArr.indexOf(2)); // It will show which value is present at this index
+console.log(myArr.includes(10)); // It checks whether the array element is present or not and gives otput as true or false
+console.log(myArr.includes(2,1)); // first value is the number means the element itself, and second value is from which index we want to start the search with and gives the true if it is present in this range of index
+console.log(myArr.indexOf(2)); // It will show the index of given element
+
+const newArr = myArr.join()  // It will bind the array and convert it to string
+console.log(myArr); // [ 0, 1, 2, 3, 4, 5 ]
+console.log(newArr);  //0,1,2,3,4,5
+console.log(typeof newArr);  // string
+
+// slice, splice
+
+// slice extract a portion of array anddo not modifies the original array
+// splice modifies the original array
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3)  // first value is the index and second value is how many values from that index is to be extracted
+console.log("C", myArr);
+console.log(myn2);
+
+
+
