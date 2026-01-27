f94e783bb238940ad85c5544eabb72f0aacca8b2
Navneet Mishra <navneet.mishra8586@gmail.com>
2026-01-27 22:11:23 +0530

    includes

--- 02_Basics/01_arrays.js
+++ 02_Basics/01_arrays.js
@@ -11,3 +11,15 @@ console.log(myArr2[1]);      //2
 console.log(myArr2.length);   //4
 
 // Array Methods
+
+myArr.push(6)  // It will add the element in last
+console.log(myArr);   //[0, 1, 2, 3,4, 5, 6]
+myArr.pop()   // It will remove the last element
+console.log(myArr); //[ 0, 1, 2, 3, 4, 5 ]
+
+myArr.unshift(9)  // It will add the element in front/first position
+console.log(myArr);  
+myArr.shift()   // It will remove the first element
+
+console.log(myArr.includes(10)); // It checks whether the array element is present or not and gives uotput as true or false
+console.log(myArr.indexOf(2)); // It will show which value is present at this index
