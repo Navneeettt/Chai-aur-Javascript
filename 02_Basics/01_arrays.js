90278d752f97481890682875c9b70435abd1e91a
Navneet Mishra <navneet.mishra8586@gmail.com>
2026-01-27 21:46:02 +0530

    array

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,13 @@
+// array
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+console.log(myArr);  // this shows the array
+console.log(myArr[0]);  // this shows the index
+
+const myHeros = ["Jatt", "Ajay"]
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr2[1]);      //2
+console.log(myArr2.length);   //4
+
+// Array Methods
