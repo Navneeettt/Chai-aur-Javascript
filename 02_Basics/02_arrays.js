89c7cb906aefcbb90d6c20417b151e3aa6fc921c
Navneet Mishra <navneet.mishra8586@gmail.com>
2026-01-28 22:45:17 +0530

    spread

--- 02_Basics/02_arrays.js
+++ 02_Basics/02_arrays.js
@@ -15,8 +15,19 @@ const all_new_heros = [...marvel_heros, ...dc_heros]   // It will create a new a
 console.log(all_new_heros);
 
 // flat function   // Returns a new array with all sub-array elements concatenated into it recursively up to the specified depth.
-const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
-const real_another_array = another_array.flat(1)         //[ 1, 2, 3, 4, 5, 6, 7, 6, 7, [ 4, 5 ] ]   // (1) is the depth
-const real_another_array = another_array.flat(2)         //[ 1, 2, 3, 4, 5, 6, 7, 6, 7,  4, 5 ]
-const real_another_array = another_array.flat(Infinity)  //[ 1, 2, 3, 4, 5, 6, 7, 6, 7,  4, 5 ]
-console.log(real_another_array);
+// const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real_another_array = another_array.flat(1)         //[ 1, 2, 3, 4, 5, 6, 7, 6, 7, [ 4, 5 ] ]   // (1) is the depth
+// const real_another_array = another_array.flat(2)         //[ 1, 2, 3, 4, 5, 6, 7, 6, 7,  4, 5 ]
+// const real_another_array = another_array.flat(Infinity)  //[ 1, 2, 3, 4, 5, 6, 7, 6, 7,  4, 5 ]
+// console.log(real_another_array);
+
+Array.isArray("Navneet")
+console.log(Array.isArray("Navneet"))  //false  // this check whether the given data is array or not
+console.log(Array.from("Navneet"))  // ['N', 'a', 'v', 'n', 'e', 'e', 't']  // from is use to create the array
+console.log(Array.from({name: "Navneet"}))  // []  // this will return empty array
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));  // [ 100, 200, 300 ]  // this will convert the variable into array
